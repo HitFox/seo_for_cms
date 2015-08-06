144d44616de15676ce0e875c62b017ffa89ae7f1
violettine <violettine@freenet.de>
2015-08-06 16:53:17 +0200

    resorted methods in crawler.rb

--- app/models/crawler.rb
+++ app/models/crawler.rb
@@ -1,5 +1,5 @@
 class Crawler
-
+  # crawles the given webpage and it's children and fetches their attributes.
   def initialize(webpage)
     @key_url = webpage
     @all_header = []
@@ -11,37 +11,80 @@ class Crawler
   end
 
   def crawl_webpage
+    internationalize_key_url(@key_url.dup)
     search_url_hash(@key_url)
     fetcher_of(@key_url)
     return_all
   end
 
-  def check_length(var)
-    temp = var.dup
-    temp.each do |text|
-      puts 0
-      if text.split(' ').size > 150
-        var << 'yes' + text
-        break
-      else
-        var << 'no'
+  def internationalize_key_url(key_url)
+    @internationalized_key_url = key_url.gsub(/\b\w+$/, '')
+  end
+
+  def search_url_hash(key_url)
+    @url_hash[key_url] = 'valid'
+    url_array = []
+    url_array << key_url
+    url_array.each do |url|
+      if @url_hash[url] == 'valid'
+        get_url_list_of(url)
+      end
+      @url_hash.keys.each do |key|
+        url_array << key
+      end
+      url_array.uniq!
+    end
+  end
+
+    def get_url_list_of(page_url)
+    begin
+      doc = Nokogiri::HTML(open(page_url))
+      doc.xpath('//@href').each do |url|
+        valid_url?(url.to_s)
+      end
+    rescue OpenURI::HTTPError => e
+      if e.message
+        @url_list[page_url] = 'invalid '+e.message
       end
     end
   end
 
-  def return_all
-    result = {}
-    result[:doc] = @doc
-    result[:all_header] = @all_header
-    result[:all_links] = @all_links_per_page.count
-    result[:p_class] = @p_class.last
-    result[:p] = @p.last
-    result[:canon_links] = @canon_links
-    result[:meta_description] = @meta_description
-    result[:title] = @title.text
-    result[:url_list] = @url_hash
+  def valid_url?(url)
+    case url
+    when /http:\/\//
+      @url_hash[url] = 'invalid'
+      return false
+    when /mailto:/
+      @url_hash[url] = 'invalid'
+      return false
+    when /.css/
+      @url_hash[url] = 'invalid'
+      return false
+    when /^#/
+      @url_hash[url] = 'invalid'
+      return false
+    when /^\//
+      check_url(url)
+    when /\/$/
+      @url_hash[url] = 'invalid'
+      return false
+    when /#{@internationalized_key_url}/
+      @url_hash[url] = 'valid'
+      return true
+    else
+      @url_hash[url] = 'invalid'
+      return false 
+    end
+  end
 
-    result
+  def check_url(url)
+    unless url.match(/http/)
+      unless url.match(/^\//)
+        url = '/'+url
+      end
+      url = @key_url+url
+    end
+    valid_url?(url)
   end
 
   def fetcher_of(page_url)
@@ -80,77 +123,30 @@ class Crawler
     end
   end
 
-
-  def valid_url?(url)
-    case url
-    when /http:\/\//
-      @url_hash[url] = 'invalid'
-      return false
-    when /mailto:/
-      @url_hash[url] = 'invalid'
-      return false
-    when /.css/
-      @url_hash[url] = 'invalid'
-      return false
-    when /^#/
-      @url_hash[url] = 'invalid'
-      return false
-    when /^\//
-      check_url(url)
-    when /\/$/
-      @url_hash[url] = 'invalid'
-      return false
-    when /www.savedo./
-      @url_hash[url] = 'valid'
-      return true
-    else
-      @url_hash[url] = 'invalid'
-      return false 
+  def check_length(var)
+    temp = var.dup
+    temp.each do |text|
+      puts 0
+      if text.split(' ').size > 150
+        var << 'yes' + text
+        break
+      else
+        var << 'no'
+      end
     end
   end
 
-  def check_url(url)
-    unless url.match(/http/)
-      unless url.match(/^\//)
-        url = '/'+url
-      end
-      url = @key_url+url
-    end
-    valid_url?(url)
-  end
-
-  def search_url_hash(key_url)
-    @url_hash[key_url] = 'valid'
-    url_array = []
-    @url_hash.keys.each do |key|
-      url_array << key
-    end
-    url_array.each do |url|
-      if @url_hash[url] == 'valid'
-        get_url_list_of(url)
-      end
-      @url_hash.keys.each do |key|
-        url_array << key
-      end
-      url_array.uniq!
-    end
-  end
-
-  def get_url_list_of(page_url)
-    # fetch all URLs of keypage's domain
-    puts page_url
-    #puts @url_list.length
-
-    begin
-      doc = Nokogiri::HTML(open(page_url))
-      doc.xpath('//@href').each do |url|
-        valid_url?(url.to_s)
-      end
-    rescue OpenURI::HTTPError => e
-      if e.message
-        @url_list[page_url] = 'invalid'+e.message
-      end
-      #break
-    end
+  def return_all
+    result = {}
+    result[:doc] = @doc
+    result[:all_header] = @all_header
+    result[:all_links] = @all_links_per_page.count
+    result[:p_class] = @p_class.last
+    result[:p] = @p.last
+    result[:canon_links] = @canon_links
+    result[:meta_description] = @meta_description
+    result[:title] = @title.text
+    result[:url_list] = @url_hash
+    result
   end
 end
